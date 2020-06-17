82e7f32bc713bb44c8c7e0b0224685ed46216589
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-17 11:54:18 +0900

    Administratorサービスクラスにinsertメソッドを実装

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,11 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
+/**
+ * Administratorのサービスクラス.
+ * 
+ * @author hiroto.kitamura
+ *
+ */
 @Service
 @Transactional
 public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * 管理者を登録.
+	 * @param administrator 登録する管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
+
 }
