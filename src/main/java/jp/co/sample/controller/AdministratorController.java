5766bbda5a4b1e9961202522a9803bd3355029f6
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-17 11:59:45 +0900

    Administratorコントローラークラスにinsertメソッドを実装

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,12 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -38,5 +40,13 @@ public class AdministratorController {
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
+	}
+	
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator admin = new Administrator();
+		BeanUtils.copyProperties(form, admin);
+		administratorService.insert(admin);
+		return "redirect:/index";
 	}
 }
