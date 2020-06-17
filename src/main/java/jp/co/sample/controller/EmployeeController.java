5fdcd6079b5d5f1793fcb0f6744c0c0cdac7e48a
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-17 15:13:01 +0900

    従業員のコントローラークラスにクリックされた従業員情報を表示する機能を実装

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package jp.co.sample.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -19,6 +22,11 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+
 	/**
 	 * 従業員一覧を出力.
 	 * 
@@ -30,5 +38,19 @@ public class EmployeeController {
 	public String showList(Model model) {
 		model.addAttribute("employeeList", employeeService.showList());
 		return "employee/list";
+	}
+
+	/**
+	 * クリックされた従業員の情報を出力.
+	 * 
+	 * @param id    クリックされた従業員のID
+	 * @param model リクエストスコープ
+	 * @return 従業員情報の画面
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		return "employee/detail";
 	}
 }
